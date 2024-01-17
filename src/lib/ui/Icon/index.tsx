216cf651aedb06d15e91873e0bb0858325384088
Tanvir Hossain <dev.tanvirhosu@gmail.com>
2024-01-17 13:44:14 +0100

    chore: expand icons

--- src/lib/ui/Icon/index.tsx
+++ src/lib/ui/Icon/index.tsx
@@ -1,14 +1,18 @@
-import { InfoIcon } from "@/lib/assets/icons/icons";
+import { InfoIcon, SearchIcon } from "@/lib/assets/icons/icons";
 import { FC } from "react";
 
-export type IconName = "info";
+export type IconName = "info" | "search";
 
 interface IconProps {
   name: IconName;
 }
 
 export const Icon: FC<IconProps> = ({ name }) => {
-  return name === "info" ? <InfoIcon /> : null;
+  return name === "info" ? (
+    <InfoIcon />
+  ) : name === "search" ? (
+    <SearchIcon />
+  ) : null;
 };
 
 export default Icon;
