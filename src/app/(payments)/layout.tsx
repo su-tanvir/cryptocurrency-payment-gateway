e809754d5e2dae0622d9e3bb7b0c9049401f166f
Tanvir Hossain <dev.tanvirhosu@gmail.com>
2024-01-18 20:43:44 +0100

    chore: remove unnecessary wallet validator

--- src/app/(payments)/layout.tsx
+++ src/app/(payments)/layout.tsx
@@ -1,6 +1,5 @@
 import { mulish } from "@/lib/assets/fonts/googleFonts";
 import "@/lib/assets/styles/globals.css";
-import Script from "next/script";
 import styles from "./layout.module.css";
 
 export default function RootLayout({
@@ -14,7 +13,6 @@ export default function RootLayout({
         <title>Pasarela de pago con criptomonedas</title>
         <meta name="description" content="Pago en línea con criptomonedas" />
         <link rel="icon" href="/icon.png" type="image/x-icon" sizes="24x24" />
-        <Script src="wallet-address-validator.min.js" />
       </head>
       <body className={mulish.className}>
         <div className={styles.app}>
--- src/features/PaymentGatewayService/Payment/GeneratePaymentQrWalletAddress/index.tsx
+++ src/features/PaymentGatewayService/Payment/GeneratePaymentQrWalletAddress/index.tsx
@@ -1,7 +1,5 @@
-import WAValidator from "multicoin-address-validator/dist/wallet-address-validator";
 import QRCode from "qrcode.react";
 import { FC } from "react";
-import { findNetworkByCryptoSymbol } from "../../Cryptocurrency/cryptocurrency.util";
 import styles from "./styles.module.css";
 
 interface GeneratePaymentQrWalletAddressProps {
@@ -12,18 +10,7 @@ interface GeneratePaymentQrWalletAddressProps {
 const GeneratePaymentQrWalletAddress: FC<
   GeneratePaymentQrWalletAddressProps
 > = ({ cryptoSymbol, cryptoAddress }) => {
-  const network = findNetworkByCryptoSymbol(cryptoSymbol);
-
-  const isValidAddress = WAValidator.validate(cryptoAddress, network);
-
   if (!cryptoAddress) return;
-  return (
-    <QRCode
-      className={`${styles.root} ${!isValidAddress ? styles.isInvalid : ""}`}
-      size={152}
-      fgColor={isValidAddress ? "#000" : "#dc2626"}
-      value={cryptoAddress}
-    />
-  );
+  return <QRCode className={styles.root} size={152} value={cryptoAddress} />;
 };
 export default GeneratePaymentQrWalletAddress;
